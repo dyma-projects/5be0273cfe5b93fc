f5dca5bd91e7ba7b739b5c4e0687dead83e163f4
Lucas Tissier <lucas.tissier@hotmail.fr>
2019-01-20 18:00:51 +0100

    projet5

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { UserService } from '../shared/services/user.service';
 
 @Component({
   selector: 'app-add-user',
@@ -8,17 +9,17 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 export class AddUserComponent implements OnInit {
   @ViewChild('input') public el: ElementRef;
 
-  constructor(
-    // il faut probablement injecter un service ici !
-  ) { }
+  constructor(private userSerice : UserService) { }
 
   ngOnInit() {
   }
+
 
   addUser() {
     const username = this.el.nativeElement.value;
     if (username) {
       // on utilise le service ici pour ajouter l'utilisateur;
+      this.userSerice.addUser(username);
       this.el.nativeElement.value = '';
     }
   }
